255a24b2867bd6614ea36e64e31824c644771e76
slashdoom <patrick.ryon@outlook.com>
2015-04-13 18:56:10 -0400

    dev

--- idbf_ad_groups.py
+++ idbf_ad_groups.py
@@ -80,7 +80,7 @@ for domain in domains:
   ldap_domain = (','.join(domain_dc_parts))
 
   ldap_entry_list = c.extend.standard.paged_search( search_base   = "dc=gruadmin,dc=gru,dc=com",
-                                                    search_filter = "(&(objectClass=person)(objectClass=user)",
+                                                    search_filter = "(&(objectCategory=person)(objectClass=user))",
                                                     search_scope  = ldap3.SUBTREE,
                                                     attributes    = ["sAMAccountName"],
                                                     paged_size    = 5,
