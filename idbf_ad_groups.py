b043d82a4e11a7426ec3f3380f1c269cc9f06ee2
slashdoom <patrick.ryon@outlook.com>
2015-04-13 19:46:21 -0400

    dev

--- idbf_ad_groups.py
+++ idbf_ad_groups.py
@@ -90,4 +90,6 @@ for domain in domains:
   for ldap_entry in ldap_entry_list:
     ldap_entry_count += 1
     ldap_samaccountname = (ldap_entry['attributes']['sAMAccountName'][0].lower())
+
+    print (c.search(ldap_domain,("(member:1.2.840.113556.1.4.1941:=(CN=%s,CN=Users,DC=YOURDOMAIN,DC=NET))" % (ldap_samaccountname)),ldap3.SUBTREE))
   print (ldap_entry_count)
