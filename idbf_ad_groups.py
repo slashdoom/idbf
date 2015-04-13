efdb15cc0f34072a4cbeb82d5afe3ed28149d86f
slashdoom <patrick.ryon@outlook.com>
2015-04-13 15:33:56 -0400

    dev

--- idbf_ad_groups.py
+++ idbf_ad_groups.py
@@ -54,6 +54,7 @@ except:
   ldap_port     = configparser.RawConfigParser.getint("LDAP","port")
   ldap_username = config["LDAP"]["username"]
   ldap_password = config["LDAP"]["password"]
+  print (ldap_port)
 #except:
   # send warning to logger
 #  logger.error("LDAP settings not found in config")
