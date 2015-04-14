4528c0d57c62f0f9eb6abae34708a32c9cabf8c7
slashdoom <patrick.ryon@outlook.com>
2015-04-14 11:07:22 -0400

    dev

--- idbf_ad_groups.py
+++ idbf_ad_groups.py
@@ -124,7 +124,7 @@ for domain in domains:
     # add primary group to list
     for ldap_group in ldap_group_list:
       if ldap_group["attributes"]["primaryGroupToken"][0] == ldap_user_primarygroupid:
-        print (ldap_group_list["attributes"]["cn"][0])
+        print (ldap_group["attributes"]["cn"][0])
         break
 
     #print ("%s: %s" % (ldap_user_samaccountname, ldap_user_memberof))
