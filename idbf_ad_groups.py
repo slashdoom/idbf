bebb66b5df8a030168235fc63fc5664c7ff8e244
slashdoom <patrick.ryon@outlook.com>
2015-04-13 22:34:52 -0400

    dev

--- idbf_ad_groups.py
+++ idbf_ad_groups.py
@@ -91,6 +91,7 @@ for domain in domains:
     ldap_entry_count += 1
     ldap_samaccountname = (ldap_entry['attributes']['sAMAccountName'][0].lower())
     #ldap_memberof = (ldap_entry['attributes']['memberOf'])
+    ldap_memberof = ldap_entry.get('attributes','memberOf')
 
     #print ("%s: %s" % (ldap_samaccountname, ldap_memberof))
 
