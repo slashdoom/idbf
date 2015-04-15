244bfee9dfde8dac1fc8f79d534678d5bcc92c71
slashdoom <patrick.ryon@outlook.com>
2015-04-15 15:37:35 -0400

    dev

--- idbf_ad_groups.py
+++ idbf_ad_groups.py
@@ -138,7 +138,8 @@ for domain in domains:
         break
 
     # convert domain\group list to comma separated string
-    ldap_user_memberof = ",".join(sorted(group_list))
+    ldap_user_memberof = "|".join(sorted(group_list))
+    ldap_user_memberof = "|" + ldap_user_memberof + "|"
 
     # add user and groups to database
     ug_db.ug_user_add(time.strftime('%Y-%m-%d %H:%M:%S'), ldap_user_samaccountname, domain, ldap_user_memberof)
