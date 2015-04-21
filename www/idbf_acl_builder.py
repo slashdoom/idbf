cb6222382d97911183d8c6bd3b2f6551cd942cdb
slashdoom <patrick.ryon@outlook.com>
2015-04-21 15:34:11 -0400

    dev

--- www/idbf_acl_builder.py
+++ www/idbf_acl_builder.py
@@ -79,7 +79,7 @@ def user_to_ip(user):
       sql_results = db_cur.fetchall()
       print (sql_results)
       for user_ip in sql_results:
-        print (user_ip)
+        print (user_ip[0])
         user_ip_list =+ user_ip
         print (user_ip_list)
       return user_ip_list
