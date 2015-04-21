dc882d4a19808a358a5551a5f3a82af0ca2e8cb8
slashdoom <patrick.ryon@outlook.com>
2015-04-21 14:49:11 -0400

    dev

--- www/idbf_acl_builder.py
+++ www/idbf_acl_builder.py
@@ -76,8 +76,9 @@ def user_to_ip(user):
     sql_query = ("SELECT ip FROM idb_view WHERE user=%s")
     db_cur.execute(sql_query, (user,))
     if db_cur.rowcount > 0: # results found
-      rv = [dict((db_cur.description[idx][0], value) for idx, value in enumerate(row)) for row in db_cur.fetchall()]
-      return (rv[0] if rv else "")
+      for user in db_cur[0]:
+        print (user)
+      return "done"
     else:
       return ""
   except:
