d4a1938f4e0a950c847c3cffb8079d69174c621e
slashdoom <patrick.ryon@outlook.com>
2015-04-21 15:37:25 -0400

    dev

--- www/idbf_acl_builder.py
+++ www/idbf_acl_builder.py
@@ -76,7 +76,7 @@ def user_to_ip(user):
     sql_query = ("SELECT ip FROM idb_view WHERE user=%s")
     db_cur.execute(sql_query, (user,))
     if db_cur.rowcount > 0: # results found
-      sql_results = db_cur.fetchone()
+      sql_results = db_cur.fetchall()
       for user_ip in sql_results:
         print (user_ip[0])
         user_ip_list = user_ip_list + user_ip[0]
