a02635807e9a4b371a3378e6350e9cb4c1cf9bf9
slashdoom <patrick.ryon@outlook.com>
2015-05-19 08:54:10 -0400

    dev

--- idbf_squid_ext_acl.py
+++ idbf_squid_ext_acl.py
@@ -83,7 +83,7 @@ try:
       sql_query = ("SELECT user, domain FROM idb_view WHERE ip=%s")
       db_cur.execute(sql_query, (re_ip.group(1),))
       for (record) in db_cur:
-        print(record[0])
+        print("OK user=" + record[0] + "@" + record[1])
         print(record[1])
     else:
       print("ERR") # ip address not found in stdin line
