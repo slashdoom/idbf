0abd496b8824ee069c7657afc0e0a54cc8fb597c
slashdoom <patrick.ryon@outlook.com>
2015-05-29 16:19:47 -0400

    dev

--- idbf_ufdb_execuser.py
+++ idbf_ufdb_execuser.py
@@ -19,20 +19,20 @@ def main(argv):
    group = ''
    user = ''
    try:
-      opts, args = getopt.getopt(argv,"hg:u:",["a_group=","a_user="])
+      opts, args = getopt.getopt(argv,"g:u:",["group=","user="])
    except getopt.GetoptError:
-      print 'test.py -g <inputfile> or -o <outputfile>'
+      print('idbf_ufdb_execuser.py -g <group> or -u <username>')
       sys.exit(2)
    for opt, arg in opts:
       if opt == '-h':
-         print 'test.py -i <inputfile> -o <outputfile>'
+         print('idbf_ufdb_execuser.py -g <group> or -u <username>')
          sys.exit()
       elif opt in ("-i", "--ifile"):
          inputfile = arg
       elif opt in ("-o", "--ofile"):
          outputfile = arg
-   print 'Input file is "', inputfile
-   print 'Output file is "', outputfile
+   print('Input file is "', inputfile)
+   print('Output file is "', outputfile)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
