5c885ccc2b5e5dbd41fd58ce16674aacd2a7eb9e
slashdoom <patrick.ryon@outlook.com>
2015-05-30 14:35:03 -0400

    dev

--- idbf_ufdb_execuser.py
+++ idbf_ufdb_execuser.py
@@ -16,23 +16,27 @@ import getopt
 import sys
 
 def main(argv):
-   group = ''
-   user = ''
-   try:
-      opts, args = getopt.getopt(argv,"g:u:",["group=","user="])
-   except getopt.GetoptError:
-      print('idbf_ufdb_execuser.py -g <group> or -u <username>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('idbf_ufdb_execuser.py -g <group> or -u <username>')
-         sys.exit()
-      elif opt in ("-g", "--group"):
-         group = arg
-      elif opt in ("-u", "--user"):
-         user = arg
-   print('Group is: ', group)
-   print('User is: ', user)
+  domain = ''
+  group = ''
+  user = ''
+  try:
+    opts, args = getopt.getopt(argv,"g:u:",["group=","user="])
+  except getopt.GetoptError:
+    print('idbf_ufdb_execuser.py -d <domain> -g <group> or -u <username>')
+    sys.exit(2)
+  for opt, arg in opts:
+    if opt in ("-h", "--help"):
+      print('idbf_ufdb_execuser.py -d <domain> -g <group> or -u <username>')
+      sys.exit()
+    elif opt in ("-d", "--domain"):
+      domain = arg
+    elif opt in ("-g", "--group"):
+      group = arg
+    elif opt in ("-u", "--user"):
+      user = arg
+    print('Domain is: ', domain)
+    print('Group is: ', group)
+    print('User is: ', user)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
